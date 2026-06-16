db2a2d3a72d31251341d97513472ba70b7e18f49
Satvik Mishra <143242723+Satvik01000@users.noreply.github.com>
2026-06-16 17:22:58 +0530

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.cpp
@@ -0,0 +1,23 @@
+class Solution {
+private:
+    bool isPalindrome(const string& s, int low, int high) {
+        while (low < high) {
+            if (s[low] != s[high]) return false;
+            low++;
+            high--;
+        }
+        return true;
+    }
+
+public:
+    bool validPalindrome(string s) {
+        int low = 0, high = s.size() - 1;
+
+        while (low < high) {
+            if (s[low] != s[high]) return isPalindrome(s, low + 1, high) || isPalindrome(s, low, high - 1);
+            low++; high--;
+        }
+
+        return true;
+    }
+};
